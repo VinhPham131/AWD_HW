cd0a1df3e2994c37b9d8493f04cfe4564eb00f72
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-10-24 18:42:07 +0700

    HW8

--- HW8/hotel.js
+++ HW8/hotel.js
@@ -1,10 +1,14 @@
-url = "https://67190e6d7fc4c5ff8f4c42d6.mockapi.io/hotel"
 // Hàm gọi API để lấy dữ liệu sản phẩm
 function getProducts(callback) {
     fetch("https://67190e6d7fc4c5ff8f4c42d6.mockapi.io/hotel")
         .then(response => response.json())
-        .then(data => callback(null, data))
-    .catch(error => callback(error, null));
+        .then(data =>  {
+            console.log('API Data:', data); // Log the response here
+            callback(null, data);
+        })
+        .catch(error => {
+            callback(error, null);
+        });
 }
     
 // Callback function để xử lý kết quả trả về từ API
@@ -14,9 +18,10 @@ function handleProducts(error, data) {
     } else {
         const productTableBody = document.getElementById('hotelTableBody');
         data.forEach(hotel => {
+            console.log('Image URL:', hotel.image); // Log the image URL for debugging
             const row = `<tr>
             <td>${hotel.id}</td>
-            <td><img src="${hotel.image}" width="100"></td>
+            <td><img src= "${hotel.img}" width="100" height = "100" ></td>
             <td>${hotel.name}</td>
             <td>${hotel.price}</td>
             <td>${hotel.location}</td>
