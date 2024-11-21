cc7492b9f85b5115259dfacbd6e30fd1b5a59538
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-11-21 17:46:50 +0700

    Fetch API

--- HW_ERD/app.js
+++ HW_ERD/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./configs/database');
+const connectDB = require('./configs/database');								
+const router = require('./routes');
+const bodyParser = require('body-parser');		
 
-app.use (express.json());
-app.use(express.urlencoded({extended: true}));
+PORT = process.env.PORT || 4000;
 
-PORT = process.env.PORT || 3011;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');									
+app.use(express.static('public')); // Để phục vụ tệp tĩnh (CSS, JS)										
+
 
 connectDB();
+router(app);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
